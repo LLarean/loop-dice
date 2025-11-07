8aec452531c6d0eafc5b9715ce33747b082cafe2
Stanislav Kozlov <llarean@ya.ru>
2025-11-07 16:16:04 +0300

    Creating a MenuView
    Creating the MenuView.cs
    The main buttons for MenuView are declared.
    Configuring the MenuView game object.
    Creating an empty EntryPoint object on the stage.

---
+++ Assets/_LoopDice/Scripts/Menu/MenuView.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LoopDice
+{
+    public class MenuView : MonoBehaviour
+    {
+        [SerializeField] private Button _mute;
+        [SerializeField] private Button _leaderboard;
+        [SerializeField] private Button _newGame;
+
+        private void Start()
+        {
+            _mute.onClick.AddListener(Mute);
+            _leaderboard.onClick.AddListener(Leaderboard);
+            _newGame.onClick.AddListener(NewGame);
+        }
+
+        private void OnDestroy()
+        {
+            _mute.onClick.RemoveAllListeners();
+            _leaderboard.onClick.RemoveAllListeners();
+            _newGame.onClick.RemoveAllListeners();
+        }
+
+        private void Mute()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Leaderboard()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void NewGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
